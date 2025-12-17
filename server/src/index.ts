6e86e792f4650d19cb186210dfcb9ffbadbbf6c1
Jatesh Parikh <jateshparikh97@gmail.com>
2025-12-17 07:26:12 +0530

    dashboardRoutes completed

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
-// import dashboardRoutes from "./routes/dashboardRoutes";
+import dashboardRoutes from "./routes/dashboardRoutes";
 // import expenseRoutes from "./routes/expenseRoutes";
 // import productRoutes from "./routes/productRoutes";
 // import userRoutes from "./routes/userRoutes";
@@ -20,12 +20,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-// app.use("/dashboard", dashboardRoutes);
+app.use("/dashboard", dashboardRoutes);
 // app.use("/products", productRoutes);
 // app.use("/users", userRoutes);
 // app.use("/expenses", expenseRoutes);
 
 const port = Number(process.env.PORT) || 3001;
 app.listen(port, "0.0.0.0", () => {
-  console.log("Server is running on port: ", port);
+  console.log("Server is running on port:", port);
 });
