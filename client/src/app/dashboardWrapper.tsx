47fbc9bb68ff764284dcb4ef9827cf0837758fe0
Jatesh Parikh <jateshparikh97@gmail.com>
2025-12-21 09:39:36 +0530

    fixed git for the client folder

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,15 @@
+// correct useEffect implementation and not the one used in the src code
+// useEffect(() => {
+//   if (isDarkMode) {
+//     document.documentElement.classList.add("dark");
+//     document.documentElement.classList.remove("light");
+//   } else {
+//     document.documentElement.classList.add("light");
+//     document.documentElement.classList.remove("dark");
+//   }
+// }, [isDarkMode]);
+
+// Loading Component
+{
+  /* <div className="w-6 h-6 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" /> */
+}
